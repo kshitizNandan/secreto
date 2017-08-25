03bf46efdeacbc8b1f63c1ac251c41b83e3a7b46
kshitiz_android <kshitiz@sphinx-solution.com>
2017-08-25 19:45:35 +0530

    Merge branch 'master' of https://github.com/kshitizNandan/secreto
    # Conflicts:
    #	Secreto/app/src/main/java/com/secreto/data/DataManager.java

--- Secreto/app/src/main/java/com/secreto/activities/CreateMessageActivity.java
+++ Secreto/app/src/main/java/com/secreto/activities/CreateMessageActivity.java
@@ -1,16 +1,25 @@
 package com.secreto.activities;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.VolleyError;
 import com.secreto.R;
 import com.secreto.base_activities.BaseActivityWithActionBar;
+import com.secreto.common.Common;
+import com.secreto.data.DataManager;
+import com.secreto.data.volley.ResultListenerNG;
+import com.secreto.responsemodel.BaseResponse;
+import com.secreto.utils.Logger;
 import com.secreto.utils.NetworkImageView;
 
 import butterknife.BindView;
@@ -25,13 +34,21 @@ public class CreateMessageActivity extends BaseActivityWithActionBar {
     TextView tvUserName;
     @BindView(R.id.etMessage)
     EditText etMessage;
+    private ProgressDialog progressDialog;
 
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ButterKnife.bind(this);
+        init();
         initView();
+    }
+
+    private void init() {
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage(getString(R.string.please_wait));
+        progressDialog.setCancelable(false);
     }
 
     private void initView() {
@@ -50,6 +67,36 @@ public class CreateMessageActivity extends BaseActivityWithActionBar {
 
     @OnClick(R.id.tvSend)
     void sendMessage() {
+        String message = etMessage.getText().toString();
+        if (TextUtils.isEmpty(message)) {
+            Toast.makeText(this, R.string.message_can_not_be_left_blank, Toast.LENGTH_SHORT).show();
+        } else {
+            if (Common.isOnline(this)) {
+                progressDialog.show();
+                String userId = "";
+                String messageClue = "";
+                DataManager.getInstance().sendMessage(userId, message, messageClue, new ResultListenerNG<BaseResponse>() {
+                    @Override
+                    public void onSuccess(BaseResponse response) {
+                        progressDialog.hide();
+                        Logger.d(TAG, "Contact Us onSuccess : " + response);
+                        finish();
+                    }
 
+                    @Override
+                    public void onError(VolleyError error) {
+                        progressDialog.hide();
+                        BaseResponse baseResponse = Common.getStatusMessage(error);
+                        if (baseResponse == null || TextUtils.isEmpty(baseResponse.getMessage())) {
+                            Toast.makeText(CreateMessageActivity.this, R.string.something_went_wrong, Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(CreateMessageActivity.this, baseResponse.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            } else {
+                Toast.makeText(this, R.string.check_your_internet_connection, Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
