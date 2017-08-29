dd33418901432ef6fad4fcd1c5029dc9975ead71
kshitiz_android <kshitiz@sphinx-solution.com>
2017-08-29 15:08:12 +0530

    commit changes after adding aminations in splash screen

--- Secreto/app/src/main/java/com/secreto/activities/SplashScreenActivity.java
+++ Secreto/app/src/main/java/com/secreto/activities/SplashScreenActivity.java
@@ -5,12 +5,14 @@ import android.graphics.PixelFormat;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
+import android.view.View;
 import android.view.Window;
+import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.secreto.R;
 import com.secreto.common.SharedPreferenceManager;
@@ -18,6 +20,10 @@ import com.secreto.common.SharedPreferenceManager;
 public class SplashScreenActivity extends AppCompatActivity {
 
     private final int SPLASH_TIME_OUT = 2000;
+    private final int TIME_OUT_DURATION = 2500;
+
+    protected AlphaAnimation fadeIn = new AlphaAnimation(0.0f , 1.0f ) ;
+    protected AlphaAnimation fadeOut = new AlphaAnimation( 1.0f , 0.0f ) ;
 
     public void onAttachedToWindow() {
         super.onAttachedToWindow();
@@ -41,12 +47,19 @@ public class SplashScreenActivity extends AppCompatActivity {
         LinearLayout linearLayout = (LinearLayout) findViewById(R.id.lLMain);
         linearLayout.clearAnimation();
         linearLayout.startAnimation(anim);
-
         anim = AnimationUtils.loadAnimation(this, R.anim.translate);
         anim.reset();
         ImageView iv = (ImageView) findViewById(R.id.imgLogo);
+        final TextView tvAppName = (TextView) findViewById(R.id.tvAppName);
         iv.clearAnimation();
         iv.startAnimation(anim);
+        tvAppName.startAnimation(fadeIn);
+        tvAppName.startAnimation(fadeOut);
+        fadeIn.setDuration(TIME_OUT_DURATION);
+        fadeIn.setFillAfter(true);
+        fadeOut.setDuration(TIME_OUT_DURATION);
+        fadeOut.setFillAfter(true);
+        fadeOut.setStartOffset(4200+fadeIn.getStartOffset());
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -61,6 +74,5 @@ public class SplashScreenActivity extends AppCompatActivity {
                 finish();
             }
         }, SPLASH_TIME_OUT);
-
     }
 }
