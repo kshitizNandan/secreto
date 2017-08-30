0d77ce68d81b14d6626d7d85de366e2ed7cf4c1d
kshitiz_android <kshitiz@sphinx-solution.com>
2017-08-30 20:45:45 +0530

    commit changes after removing useless code

--- Secreto/app/src/main/java/com/secreto/activities/FindUserActivity.java
+++ Secreto/app/src/main/java/com/secreto/activities/FindUserActivity.java
@@ -1,19 +1,20 @@
 package com.secreto.activities;
 
+import android.app.ProgressDialog;
 import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
 import android.text.TextUtils;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.Toast;
+import android.widget.ViewFlipper;
 
 import com.android.volley.VolleyError;
 import com.secreto.R;
@@ -24,7 +25,6 @@ import com.secreto.common.Constants;
 import com.secreto.common.SharedPreferenceManager;
 import com.secreto.data.DataManager;
 import com.secreto.data.volley.ResultListenerNG;
-import com.secreto.mediatorClasses.TextWatcherMediator;
 import com.secreto.responsemodel.BaseResponse;
 import com.secreto.responsemodel.UserResponse;
 import com.secreto.utils.CustomProgressDialog;
@@ -33,30 +33,31 @@ import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
-public class FindUserActivity extends BaseActivityWithActionBar implements View.OnClickListener, SearchView.OnQueryTextListener, SearchManager.OnCancelListener {
+public class FindUserActivity extends BaseActivityWithActionBar implements View.OnClickListener, SearchView.OnQueryTextListener, SearchManager.OnDismissListener {
 
     private static final int RC_SEND_MESSAGE = 200;
-    @BindView(R.id.etSearch)
-    EditText edtSearch;
-    @BindView(R.id.imgSearch)
-    ImageView imgSearch;
-    @BindView(R.id.imgSearchCross)
-    ImageView imgCross;
+    @BindView(R.id.viewFlipper)
+    ViewFlipper viewFlipper;
+    @BindView(R.id.input_layout_email_editText)
+    TextInputLayout textInputLayoutEmail;
+    @BindView(R.id.etEmail)
+    EditText etEmail;
     @BindView(R.id.recycler_view_search)
     RecyclerView recyclerViewSearch;
+    private boolean flipFlag;
 
     private SearchUserAdapter searchAdapter;
     private ArrayList<Object> items = new ArrayList<>();
-    private CustomProgressDialog progressDialog;
     private FindUserActivity mActivity;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ButterKnife.bind(this);
         init();
-        findUserApiCall();
     }
 
     @Override
@@ -66,37 +67,31 @@ public class FindUserActivity extends BaseActivityWithActionBar implements View.
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        // Inflate menu to add items to action bar if it is present.
-        inflater.inflate(R.menu.menu_search, menu);
-        // Associate searchable configuration with the SearchView
-        SearchManager searchManager =
-                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        SearchView searchView =
-                (SearchView) menu.findItem(R.id.menu_search).getActionView();
-        searchView.setSearchableInfo(
-                searchManager.getSearchableInfo(getComponentName()));
+        getMenuInflater().inflate(R.menu.menu_search, menu);
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.menu_search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
         searchView.setOnQueryTextListener(this);
-        searchManager.setOnCancelListener(this);
+        searchManager.setOnDismissListener(this);
         return true;
     }
 
 
     private void init() {
+        progressDialog = new CustomProgressDialog(this);
         mActivity = this;
         recyclerViewSearch.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
         searchAdapter = new SearchUserAdapter(items, this);
         recyclerViewSearch.setAdapter(searchAdapter);
     }
 
-
-    //    @OnClick(R.id.btnNext)
+    @OnClick(R.id.btnSubmit)
     void findUserApiCall() {
-        String userName = edtSearch.getText().toString().trim();
+        String userName = etEmail.getText().toString().trim();
         if (TextUtils.isEmpty(userName)) {
-            //   input_layout_userName_editText.setError(getString(R.string.user_name_can_not_be_left_blank));
+            textInputLayoutEmail.setError(getString(R.string.user_name_can_not_be_left_blank));
         } else if (userName.equalsIgnoreCase(SharedPreferenceManager.getUserObject().getName()) || userName.equalsIgnoreCase(SharedPreferenceManager.getUserObject().getEmail())) {
-            // input_layout_userName_editText.setError(getString(R.string.you_can_not_send_message_to_yourself));
+            textInputLayoutEmail.setError(getString(R.string.you_can_not_send_message_to_yourself));
         } else {
             if (Common.isOnline(mActivity)) {
                 progressDialog.show();
@@ -116,8 +111,7 @@ public class FindUserActivity extends BaseActivityWithActionBar implements View.
                             overridePendingTransition(R.anim.in_from_right_animation, R.anim.out_from_left_animation);
                             finish();
                         } else {
-
-//                            input_layout_userName_editText.setError(response.getMessage());
+                            textInputLayoutEmail.setError(response.getMessage());
                         }
                     }
 
@@ -141,7 +135,6 @@ public class FindUserActivity extends BaseActivityWithActionBar implements View.
 
     @Override
     public void onClick(View view) {
-
     }
 
     @Override
@@ -151,11 +144,17 @@ public class FindUserActivity extends BaseActivityWithActionBar implements View.
 
     @Override
     public boolean onQueryTextChange(String newText) {
-        return false;
+        if (!flipFlag) {
+            viewFlipper.setDisplayedChild(1);
+            flipFlag = true;
+        }
+        return true;
     }
 
-    @Override
-    public void onCancel() {
 
+    @Override
+    public void onDismiss() {
+        flipFlag = false;
+        viewFlipper.setDisplayedChild(0);
     }
 }
